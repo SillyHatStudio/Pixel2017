c05e8e344e20bf13118f7bf4356dd048ced16b04
Coolyoda <paturbofire@hotmail.com>
2017-04-04 21:41:26 -0400

    Unity

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class Player : Entity
+{
+    public InputDevice Device { get; set; }
+
+
+    protected override void Start()
+    {
+        base.Start();     
+    }   
+
+    void Update()
+    { 		
+    
+    }
+
+
+
+  
+
+}
