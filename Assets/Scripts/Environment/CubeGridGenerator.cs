c29015110acd80a3c4691695ac7603ef673c3a4b
yannismrad <yannismrad@free.fr>
2017-04-07 01:10:08 -0400

    Spawn point attemps + cube coloring

--- Assets/Scripts/Environment/CubeGridGenerator.cs
+++ Assets/Scripts/Environment/CubeGridGenerator.cs
@@ -8,7 +8,13 @@ public class CubeGridGenerator : MonoBehaviour
     public int width;
     public int height;
     private int defaultWidth = 12, defaultHeight = 10;
+    private int spawnPointWidth = 2, spawnPointHeight =  1;
     public GameObject MapCubePrefab;
+    private int m_OffsetToLeftBorder = 2;
+
+    public Vector2 spawnPointP1, spawnPointP2;
+
+    [HideInInspector]
     public GameObject[,] cubegrid;
 
     private void Awake()
@@ -28,19 +34,70 @@ public class CubeGridGenerator : MonoBehaviour
     // Update is called once per frame
     private void Start()
     {
-        for(int i= (int)transform.position.x; i < width; i++)
+        //Fill grid
+        for (int i = (int)transform.position.x; i < width; i++)
         {
-            for (int  j = (int)transform.position.y; j < height; j++)
+            for (int j = (int)transform.position.y; j < height; j++)
             {
                 if (MapCubePrefab)
                 {
                     //Place a cube on x,y
                     GameObject cube = PoolManager.instance.GetPoolObject(MapCubePrefab.name);
-                    cube.transform.position = new Vector2(i,j);
+                    cube.transform.position = new Vector2(i, j);
                     cubegrid[i, j] = cube;
                 }
             }
         }
+
+        PlacePlayerSpawnPoints();
+    }
+
+    public void PlacePlayerSpawnPoints()
+    {
+        /*float quarter = height / 4f;
+        float upperSpawnY = Mathf.FloorToInt(height / 2f + quarter);
+        float lowerSpawnY = Mathf.FloorToInt(height / 2f - quarter);
+
+        for(int i=0; i< spawnPointWidth; i++)
+        {
+            for (int j = 0; j < spawnPointWidth; j++)
+            {
+                cubegrid[m_OffsetToLeftBorder + i, (int)upperSpawnY - j].GetComponent<CubeColoring>().SetMaterialColor(Color.black);
+                cubegrid[m_OffsetToLeftBorder + i, (int)lowerSpawnY - j].GetComponent<CubeColoring>().SetMaterialColor(Color.blue);
+            }    
+        }*/
+
+
+        //To continue 
+
+
+        /*if(spawnPointP1.x >= 0 && spawnPointP1.x < width)
+        {
+            //If the spawnpoint overflows the right limit of the map, offet it to the left
+            if(spawnPointP1.x + spawnPointWidth > width)
+            {
+                var leftOffset = spawnPointWidth - Mathf.Abs(width - spawnPointP1.x);
+                spawnPointP1.x = width - leftOffset;
+            }
+
+            //If overflow on left  => offsets to the right
+            if (spawnPointP1.x + spawnPointWidth > width)
+            {
+                var rightOffset = spawnPointWidth - Mathf.Abs(0 + spawnPointP1.x);
+                spawnPointP1.x = rightOffset;
+            }
+
+            //If overflow top  => offsets to bottom
+            if (spawnPointP1.y + spawnPointHeight > height)
+            {
+                var botOffset = height - Mathf.Abs(spawnPointP1.y);
+                //spawnPointP1.x = rightOffset;
+            }
+        } */
+
+        
+
+
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Gameplay/Entities/CubeColoring.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class CubeColoring : MonoBehaviour {
+
+    private EnumTypes.PlayerEnum m_OwnerNumber;
+    private Color m_CurrentColor;
+    private Material m_Material;
+
+    void Awake()
+    {
+        m_OwnerNumber = EnumTypes.PlayerEnum.Unassigned;
+        m_CurrentColor = Color.gray;
+
+        m_Material = GetComponent<MeshRenderer>().material;
+
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SetOwner(int ownerNumber)
+    {
+        switch (ownerNumber)
+        {
+            case 0:
+                m_OwnerNumber = EnumTypes.PlayerEnum.P1;
+                m_CurrentColor = EnumTypes.PlayerColors.ColorP1;
+                SetMaterialColor(EnumTypes.PlayerColors.ColorP1);
+                break;
+
+            case 1:
+                m_OwnerNumber = EnumTypes.PlayerEnum.P2;
+                m_CurrentColor = EnumTypes.PlayerColors.ColorP2;
+                SetMaterialColor(EnumTypes.PlayerColors.ColorP2);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public void SetMaterialColor(Color color)
+    {
+        m_Material.color = color;
+    }
+}
